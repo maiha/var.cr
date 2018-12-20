f4251cc0ceb5597a31cd470f956e66ba8371250e
maiha <maiha@wota.jp>
2018-12-21 04:43:53 +0900

    spec: ensures `foo = nil` is NOP

--- spec/type_nilable_spec.cr
+++ spec/type_nilable_spec.cr
@@ -4,6 +4,10 @@ private class MyClass
   var foo : String? = nil
 end
 
+private class WithDefault
+  var host : String = "localhost"
+end
+
 describe "var foo : String? = nil  # TypeDeclaration" do
   context "(without initialization)" do
     obj = MyClass.new
@@ -36,5 +40,14 @@ describe "var foo : String? = nil  # TypeDeclaration" do
         obj.foo?.should eq("a")
       end
     end
+  end
+end
+
+describe "var host : String = 'localhost'  # TypeDeclaration" do
+  it "ignores nil" do
+    obj = WithDefault.new
+    obj.host.should eq("localhost")
+    obj.host = nil
+    obj.host.should eq("localhost")
   end
 end
